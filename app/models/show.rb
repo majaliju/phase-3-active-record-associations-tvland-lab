5c3e7fad43f9606cdd4c018c9ab6caeda20edab5
majaliju <majalijunyc@gmail.com>
2022-08-04 21:13:20 -0400

    commit tree c2d9db9ddc7186884ab3ee3cb72d10b3f11ef684

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+
+  def actors_list
+    self.characters.collect do |character|
+      "#{character.actor.first_name} #{character.actor.last_name}"
+    end
+  end
 end
